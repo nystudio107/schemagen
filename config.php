ea492ce7bbf3eae78a4731cce6de63d132176f0c
Andrew Welch <andrew@keluli.local>
2022-05-03 13:56:52 -0400

    refactor: Separate the templates out by Craft version

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
 const CRAFT_VERSION = 4;
 const MODEL_NAMESPACE = 'nystudio107\\seomatic\\models\\jsonld';
-const TRAIT_TEMPLATE = 'templates/trait.php.template';
-const MODEL_TEMPLATE = 'templates/model.php.template';
-const FIELD_TEMPLATE = 'templates/field.template';
+const TEMPLATES_DIR = 'templates/';
+const TRAIT_TEMPLATE = 'trait.php.template';
+const MODEL_TEMPLATE = 'model.php.template';
+const FIELD_TEMPLATE = 'field.template';
 const OUTPUT_FOLDER = 'output/';
